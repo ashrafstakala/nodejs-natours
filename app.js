f1de932010a277f73cf8b2f6b57c9bcb08506dd4
ashrafstakala <ashrafstakala@gmail.com>
2022-02-15 21:31:25 +0200

    refactor routers

--- app.js
+++ app.js
@@ -142,27 +142,28 @@ const deleteUser = (req, res) => {
 };
 
 // 3) ROUTES
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
-app
-  .route('/api/v1/users')
-  .get(getAllUsers)
-  .post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) SERVER START
 const port = 3000;
