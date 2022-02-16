205a3a5b77b705f6d2aaed43e91d55d392300b5d
ashrafstakala <ashrafstakala@gmail.com>
2022-02-16 18:19:59 +0200

    delete docs in mongodb via endpoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,14 +23,12 @@ exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
 
-    if (tour) {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
-    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
   } catch (err) {
     res.status(404).json({
       staus: 'fail',
@@ -45,7 +43,6 @@ exports.createTour = async (req, res) => {
 
     res.status(201).json({
       status: 'success',
-      requestedAt: req.requestTime,
       data: {
         tour: newTour,
       },
@@ -79,10 +76,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
