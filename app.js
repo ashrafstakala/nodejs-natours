53cd32ec8bf6777b93d48775a9c220e33030ac6d
ashrafstakala <ashrafstakala@gmail.com>
2022-02-17 15:25:19 +0200

    handle unhandled routes

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// If we reach this point, it means the above routes were not able to handle the request from client
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
