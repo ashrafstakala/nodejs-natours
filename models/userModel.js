d88389b57a0c2df40ce2626cf92dd5cebb0593f9
ashrafstakala <ashrafstakala@gmail.com>
2022-02-18 16:03:21 +0200

    route protection aut logic

--- models/userModel.js
+++ models/userModel.js
@@ -37,6 +37,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same.',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -58,6 +59,18 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp; // returns true if changed after token issued, else false
+  }
+  // False means NOT changed
+  return false;
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
