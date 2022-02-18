7448216bb22bf356a321f528cbcfd83fa8f03e02
ashrafstakala <ashrafstakala@gmail.com>
2022-02-18 11:38:31 +0200

    password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -27,7 +28,26 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     minlength: 8,
+    validate: {
+      // This only works on CREATE & SAVE - keep in mind when updating passwords
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwors are not the same.',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the password confrim field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
