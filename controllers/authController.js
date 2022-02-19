1c7200f069755496d0854095c6202df5f6fb8548
ashrafstakala <ashrafstakala@gmail.com>
2022-02-19 14:42:48 +0200

    create password reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,3 +104,19 @@ exports.restrictTo =
     }
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1. Get user based on POST email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address.', 404));
+  }
+
+  // 2. Generate the random password reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. Send it to the user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
