8bae69329f89c95e139e45b613620edcccfcf203
ashrafstakala <ashrafstakala@gmail.com>
2022-02-19 19:46:53 +0200

    data input sanitization security

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,16 +13,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARE
-
-// Set security HTTP headers
+// M1: Set security HTTP headers
 app.use(helmet());
 
-// Development logging
+// M2: Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit requests from same API
+// M3: Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -28,13 +29,19 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Body parser, reading data from body into req.body
+// M4: Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
-// Serving static files
+// M5: Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// M6: Data sanitization against XSS attacks
+app.use(xss());
+
+// M6: Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Test middleware
+// M7: Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
