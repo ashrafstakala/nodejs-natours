8c68b5b618d5f595915d4c1dc1582f084f9b7b30
ashrafstakala <ashrafstakala@gmail.com>
2022-02-21 19:16:21 +0200

    add /me endpoint

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -87,16 +87,3 @@ exports.getAll = (Model) =>
       },
     });
   });
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// });
