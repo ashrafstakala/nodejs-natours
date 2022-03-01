9f8a193c164538cbbaef46d0dcbdb4c63be78cdf
ashrafstakala <ashrafstakala@gmail.com>
2022-03-01 23:55:11 +0200

    error pages

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use(
 // M8: Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
